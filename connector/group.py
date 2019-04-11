4c15163f7386212809ba703a5c6826faf0789e03
Charles Prevot <cprevot@fortinet.com>
2019-04-11 12:39:08 +0200

    [GROUP] class inheritance

--- connector/group.py
+++ connector/group.py
@@ -9,11 +9,14 @@ class Group(FMG_object):
     FMG_object.__init__(self, name, adom, ipv6)
     self.__subgrp = subgrp
     self.__subnets = subnets
+    self.__member = list()
 
   def _FMG_create(self):
     helpers.logger.info("Creating group " + self.get_name() + " on FMG")
     urlpf = "pm/config/" + self.get_adom()
     url = urlpf + "/obj/firewall/addrgrp"
+    if self.is_ipv6():
+      url += "6"
     member = list()
 
     for sub in self.__subnets:
@@ -42,8 +45,11 @@ class Group(FMG_object):
     helpers.logger.info("Updating group " + self.get_name() + " on FMG")
     urlpf = "pm/config/" + self.get_adom()
     url = urlpf + "/obj/firewall/addrgrp"
+    if self.is_ipv6():
+      url += "6"
+    url += "/" + self.get_name()
+    
     member = list()
-
     for sub in self.__subnets:
       member.append(sub.get_name())
     debug = "\n\tsubnets: " + str(member)
@@ -52,19 +58,17 @@ class Group(FMG_object):
       member.append(grp.get_name())
     debug += "\n\tsubgroups: " + str(self.__subgrp) + "\n\tadom: " + self.get_adom()
     helpers.logger.debug(debug)
-    obj = {
-      'name': self.get_name(),
-      'comment': 'Created by efficientIP',
-      'color': 13,
-      'member': member
-      }
+    obj = { 'member': member }
     helpers.logger.debug("URL: " + url)
-    code, data = helpers.api.add(url, obj)
+    code, data = helpers.api.update(url, obj)
 
     if code['code'] != 0:
       raise RuntimeError(code['message'])
 
     return code,data
+
+  def FMG_delete(self):
+    pass
 
   def _is_new(self):
     status,data = helpers.firewall_table(self.get_adom(), self.get_name())
